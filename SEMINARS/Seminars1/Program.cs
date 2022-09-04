6d0615d7b594d2274f7b61c9627e79401cf068c8
Oleg Logvinov <boleg11v@mail.ru>
2022-09-04 13:51:17 +0300

    added info seminars 1

---
+++ SEMINARS/Seminars1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+// Задача 1. Напишите программу, которая на вход принимает число и выдает его квадрат (число, умноженное само на себя)
+
+Console.Write("Inhut a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int qard = num * num;
+
+Console.WriteLine($"The square of {num} is {qard}");
+
+*/
+/*
+// Задача 2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+Console.Write("Input ferst number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int qard2 = n2 * n2;
+
+if (n1 == qard2)
+{
+    Console.WriteLine($"{n1} is qard2 of {n2}");
+}
+else
+{
+    Console.WriteLine($"{n1} is not qard2 of {n2}");
+}
+*/
