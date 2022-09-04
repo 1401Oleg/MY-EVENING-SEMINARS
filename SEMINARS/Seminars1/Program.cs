7181108ed1748a04cd72f2b9b284eb1739505986
Oleg Logvinov <boleg11v@mail.ru>
2022-09-04 22:10:38 +0300

    added info about four tests

--- SEMINARS/Seminars1/Program.cs
+++ SEMINARS/Seminars1/Program.cs
@@ -28,3 +28,28 @@ else
     Console.WriteLine($"{n1} is not qard2 of {n2}");
 }
 */
+/*
+// задача 3. Напишите программу, которая на вход принимает одно чило (N), а на выходе показывает все целые числа в промежутке от отрицательной версии этого числа до самого этого числа.
+
+Console.Write("Input integer positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = number * (-1);
+
+while (current <= number)
+{
+   Console.Write(current + " ");
+   current++; 
+   }
+*/
+/*
+// задача 4. Напишите программу, которая на вход принимает трехзначное число, а на выходе дает последнюю цифру этого числа
+
+Console.Write("Input integer three digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ed = number % 10;
+
+Console.WriteLine($"Last digit of {number} is {ed}");
+
+*/
