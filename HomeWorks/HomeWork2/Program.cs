a39031f53516f9e72a51b550dc80ec234639b640
Oleg Logvinov <boleg11v@mail.ru>
2022-09-11 15:20:15 +0300

    added info about test 10 and test 13

---
+++ HomeWorks/HomeWork2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+Console.Write("Input three digit number: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("Вторая цифра числа -> "+stringNumber[1]);
+
+*/
+
+/*
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input number: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("Three digit -> " + anyNumberText[2]);
+}
+    else 
+{
+    Console.WriteLine("-> no third digit");
+}    
+*/
+
---
+++ SEMINARS/Seminar2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// Задача 1. Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру данного числа.
+/*
+int FindBiggestDigit(int number);
+
+{
+int ed = number % 10;
+int dec number / 10;
+
+if (ed >dec) 
+{
+return ed;
+}
+else
+{
+return dec;
+}
+}
+*/
+
+
+
+// Задача 1. Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру данного числа.
+
+int FindBiggestDigit(int number);    
+
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int max;
+
+    if (ed >dec) 
+      max = ed;
+
+    else
+      max = dec;
+
+    return max;
+}
+
+
+Console.Write("Random two digit number is ");
+
+int randomNumber = new Random().Next(10,100);
+
+int biggestDigit = FindBiggestDigit(randomNumber);
+
+Console.WriteLine($"Biggest digit of {randomNumber} is {biggestDigit}");
+
