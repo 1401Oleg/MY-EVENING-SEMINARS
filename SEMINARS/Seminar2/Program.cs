d6d783376a5c765def5a23ad165aa2dd16dda95c
Oleg Logvinov <boleg11v@mail.ru>
2022-09-22 10:48:55 +0300

    added info about homework3

--- SEMINARS/Seminar2/Program.cs
+++ SEMINARS/Seminar2/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 // Задача 1. Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру данного числа.
 /*
-int FindBiggestDigit(int number);
+int FindBiggestDigit(int number)
 
 {
 int ed = number % 10;
-int dec number / 10;
+int dec = number / 10;
 
 if (ed >dec) 
 {
@@ -16,20 +16,22 @@ else
 return dec;
 }
 }
+int randomNumber = new Random().Next(10,100);
+
+int biggestDigit = FindBiggestDigit(randomNumber);
+Console.WriteLine($"Biggest digit of {randomNumber} is {biggestDigit}");
 */
 
-
-
 // Задача 1. Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру данного числа.
+/*
+int FindBiggestDigit(int number)    
 
-int FindBiggestDigit(int number);    
+{    
+  int ed = number % 10;
+  int dec = number / 10;
+  int max;
 
-{
-    int ed = number % 10;
-    int dec = number / 10;
-    int max;
-
-    if (ed >dec) 
+    if (ed > dec) 
       max = ed;
 
     else
@@ -39,11 +41,81 @@ int FindBiggestDigit(int number);
 }
 
 
-Console.Write("Random two digit number is ");
-
 int randomNumber = new Random().Next(10,100);
 
 int biggestDigit = FindBiggestDigit(randomNumber);
-
 Console.WriteLine($"Biggest digit of {randomNumber} is {biggestDigit}");
 
+*/
+
+// Задача 2. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+/*
+int CutNumber(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+
+    int result = sot * 10 + ed;
+    return result;
+  
+}
+int randomNumber = new Random().Next(100,1000);
+
+int newNumber = CutNumber(randomNumber);
+Console.WriteLine($"Neww version of {randomNumber} is {newNumber}");
+*/
+
+// Задача 3. Напишите программу, которая будет на вход принимать два числа и выводить, является ли второе число кратным первому, если нет, то выводить остаток от деления.
+/*
+void IsDivisionable(int num1, int num2)
+{
+     int reminder = num2 % num1;
+     if(reminder==0)
+     {
+         Console.WriteLine($"{num2} is IsDivisionable{num1} ");
+     }
+     else
+     {
+         Console.WriteLine($"{num2} is not IsDivisionable{num1}. Reminder {reminder} ");
+     }
+}
+
+Console.Write("Input ferst number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+IsDivisionable(n1,n2);
+*/
+
+// Задача 4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+/*
+bool IsDivis(int number)
+{
+     if(number % 7 == 0  && number % 23 == 0)
+     {
+         return(true);
+     }
+     else
+     {
+         return false;  
+     }    
+}
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool IsDivison = IsDivis(n);
+Console.WriteLine(IsDivison);
+*/
+// Задача 4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+bool IsDivis(int number)
+{
+     return number % 7 == 0  && number % 23 == 0;
+                   
+}
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool IsDivison = IsDivis(n);
+Console.WriteLine(IsDivison);
